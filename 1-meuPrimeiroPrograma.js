0aa586c672ba4b7f945e115e92e05337bde3a4a4
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-20 11:13:23 -0400

    salvando variaveis na memoria

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,9 @@
+console.log("Meu Primeiro programa: trabalhando com varáveis");
+
+const idade = 26;
+
+console.log(idade)
+console.log(idade + 3);
+
+const idadeSomada = idade + 2;
+console.log(idadeSomada)
