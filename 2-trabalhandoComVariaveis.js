86c251dba847a76fa8d614f03653144365672f71
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-20 11:29:20 -0400

    aprendendo tipos de declaracoes

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,13 @@
+//Console.log("Trabalhando com variáveis")
+//JS É case sensitive
+console.log("Trabalhando com variáveis")
+
+//Idade = 30
+const idade = 29;
+const nome = "Guilherme";
+//let ano = 2022;
+
+//não fazer
+mes = 'Janeiro';
+
+console.log(idade);
