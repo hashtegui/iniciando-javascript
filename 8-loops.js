97b191436b5a556db48c10e29432784cc4c03b47
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-21 10:11:59 -0400

    utilizando o break

--- 8-loops.js
+++ 8-loops.js
@@ -14,12 +14,13 @@ console.log("\n Destinos Possiveis");
 const podeComprar = (idadeComprador >= 18 || estaAcompanhada == true);
 
 let contador = 0;
-
+let destinoExiste = false;
 while (contador < 3) {
     if (listaDeDestinos[contador] == destino) {
-        console.log("Destino existe");
-    } else {
-        console.log("Destino não existe")
+        destinoExiste = true;
+        break;
     }
     contador += 1;
 }
+
+console.log("Destino existe: ", destinoExiste);
